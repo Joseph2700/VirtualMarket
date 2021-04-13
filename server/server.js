d39dc19d1f7cc0abc7139656fc8bf47622a98b00
axellopez89 <axellpz89@hotmail.com>
2021-04-13 19:51:24 -0400

    Now server connects to DB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+require("dotenv").config();
+
+var mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DATABASE,
+    timezone: process.env.RDS_TIMEZONE
+});
+connection.connect(function (err){
+    if (err){
+        console.error("Database connection failed: " + err.stack);
+        return;
+    }
+    console.log("Connected to database.")
+})
 
 app.listen(8080, function() {
     console.log('Listening on 8080'); 
@@ -8,3 +28,5 @@ app.listen(8080, function() {
 app.get('/api', function(req, res) {
     res.send('Bravo Six, going dark');
 })
+
+module.exports = connection;
