e25691e76593046dc0b8077a1c2e1cc6d87e030a
DiegoJavierJ <49220228+DiegoJavierJ@users.noreply.github.com>
2021-04-14 09:44:42 -0400

    userModel and Controller created

---
+++ server/Controllers/UsuarioController.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const usuarioModel = require("../Models/UsuarioModel")
+
+exports.Crear = (req, res) => {
+    if (!req.body) {
+      res.status(400).send({
+        message: "El contenido no puede estar vacio!",
+      });
+    }
+    const nuevoUsuario = new Usuario(
+      req.body.NombreUsuario,
+      req.body.Contraseña,
+    );
+    Usuario.Crear(nuevoUsuario, (err, data) => {
+      if (err){
+        res.status(500).send({
+          message:
+             err.message || "Ocurrio un Error al crear el usuario."
+         });
+         console.log(err);
+      } else {
+        res.send(data);
+      }
+    });
+  };
+
+  exports.getAllUsuario = (req, res) => {
+    usuarioModel.getAllUsuario((err, Usuario) => {
+      console.log("here");
+  
+      console.log("Usuarios", Usuario);
+      res.send(Usuario);
+    });
+  };
--- server/Models/UsuarioModel.js
+++ server/Models/UsuarioModel.js
@@ -8,3 +8,31 @@ var usuario = function(
     this.Contrasena = uContrasena
 }
 
+usuario.Crear = (newUser, result) => {
+    connection.query(`call AgregarUsuario( "?", "?");`
+      [newUser, result]  
+    , true, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      console.log("Usuario creado exitosamente");
+    });
+  };
+  
+  usuario.getAllUsuario = (result) => {
+    const sql = `SELECT * FROM usuario;`;
+    db.query(sql, (err, res) => {
+      if (err) {
+        console.log("Hay un error.", err);
+        result(null, err);
+        return;
+      } else {
+        console.log("Usuarios encontrados: ");
+        result(null, res);
+      }
+    });
+  };
+  
+  module.exports = usuario;
