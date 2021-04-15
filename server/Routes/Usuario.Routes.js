bcbbd4e6f9b5f1f6d7881906ba1140bc1d93a707
DiegoJavierJ <49220228+DiegoJavierJ@users.noreply.github.com>
2021-04-14 20:42:47 -0400

    Resolving connection

--- server/Routes/Usuario.Routes.js
+++ server/Routes/Usuario.Routes.js
@@ -1,9 +1,9 @@
-const express = requiere("express")
+const express = require("express")
 const router = express.Router()
 
-const UsuarioController = require('../Controllers/Usuario.Controller')
+const UsuarioController = require('../Controllers/UsuarioController')
 
-router.get("/getUsuario", UsuarioController.getAllUsuarios)
+router.get("/getUsuario", UsuarioController.getAllUsuario)
 router.post("/CrearU", UsuarioController.Crear)
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
 require("dotenv").config();
+const cors = require('cors')
+const router = express.Router()
 
 var mysql = require('mysql');
-const connection = mysql.createConnection({
+const connections = mysql.createConnection({
     host: process.env.RDS_HOSTNAME,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
@@ -13,7 +16,7 @@ const connection = mysql.createConnection({
     database: process.env.RDS_DATABASE,
     timezone: process.env.RDS_TIMEZONE
 });
-connection.connect(function (err){
+connections.connect(function (err){
     if (err){
         console.error("Database connection failed: " + err.stack);
         return;
@@ -21,12 +24,23 @@ connection.connect(function (err){
     console.log("Connected to database.")
 })
 
+
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// const usuarioRoutes = require("./Routes/Usuario.Routes")
+// app.use("/usuario", usuarioRoutes)
+
 app.listen(8080, function() {
     console.log('Listening on 8080'); 
 })
+app.get("/usuario", (req, res)=>{
+    connections.query("SELECT * FROM virtualmarket.tbl_usuario;", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      });
 
-app.get('/home', function(req, res) {
-    res.send('Bravo Six, going dark');
 })
-
-module.exports = connection;
+module.exports = connections;
