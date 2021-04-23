5ac85b774249d517ff26a7c5ecb37aec359d7baa
axellopez89 <axellpz89@hotmail.com>
2021-04-23 19:51:49 -0400

    New MSSQL now uses dotenv

--- server/connection.js
+++ server/connection.js
@@ -1,22 +1,11 @@
 const sql = require('mssql');
-//module.exports = () => {
-//    const connections = mysql.createConnection({
-//        host: process.env.RDS_HOSTNAME,
-//        user: process.env.RDS_USERNAME,
-//        password: process.env.RDS_PASSWORD,
-//        port: process.env.RDS_PORT,
-//        database: process.env.RDS_DATABASE,
-//        timezone: process.env.RDS_TIMEZONE
-//    });
-
-//    return connections;
-//}
+require("dotenv").config({ path: "../.env" });
 
 const config = {
-	user: "sqlserver", 
-	password: "Virtualmarket01", 
-	server: "35.229.21.228", 
-	database: "Virtualmarket",
+	user: process.env.RDS_USERNAME, 
+	password: process.env.RDS_PASSWORD, 
+	server: process.env.RDS_HOSTNAME, 
+	database: process.env.RDS_DATABASE,
 };
 
 sql.connect(config, function (err){
